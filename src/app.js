1776873b6a1564a1a1efd5516abc19b8935d14b6
rochelleaguila <143100232+rochelleaguila@users.noreply.github.com>
2023-10-16 18:31:21 +0000

    updating efficient code changes

--- src/app.js
+++ src/app.js
@@ -1,23 +1,14 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = () => {
-  //write your code here
-  document.querySelector("#bttn").addEventListener("click", () => {
-    document.querySelector("#excuse").innerHTML = generateExcuse();
-  });
-  console.log("Hello Rigo from the console!");
-};
-
-let generateExcuse = () => {
-  let who = ["The dog", "My grandma", "Bob's turtle", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car"];
-  let when = [
+document.addEventListener("DOMContentLoaded", () => {
+  const who = ["The dog", "My grandma", "Bob's turtle", "My bird"];
+  const action = ["ate", "peed", "crushed", "broke"];
+  const what = ["my homework", "the keys", "the car"];
+  const when = [
     "before the class",
     "right on time",
     "when I finished",
@@ -25,18 +16,21 @@ let generateExcuse = () => {
     "while I was praying"
   ];
 
-  let whoIndx = Math.floor(Math.random() * who.length);
-  let actnIndx = Math.floor(Math.random() * action.length);
-  let whatIndx = Math.floor(Math.random() * what.length);
-  let whenIndx = Math.floor(Math.random() * when.length);
+  const generateExcuse = () => {
+    const whoIndx = Math.floor(Math.random() * who.length);
+    const actnIndx = Math.floor(Math.random() * action.length);
+    const whatIndx = Math.floor(Math.random() * what.length);
+    const whenIndx = Math.floor(Math.random() * when.length);
 
-  return (
-    who[whoIndx] +
-    " " +
-    action[actnIndx] +
-    " " +
-    what[whatIndx] +
-    " " +
-    when[whenIndx]
-  );
-};
+    return `${who[whoIndx]} ${action[actnIndx]} ${what[whatIndx]} ${when[whenIndx]}`;
+  };
+
+  const bttn = document.querySelector("#bttn");
+  const excuseElement = document.querySelector("#excuse");
+
+  bttn.addEventListener("click", () => {
+    excuseElement.textContent = generateExcuse();
+  });
+
+  console.log("Hello Rigo from the console!");
+});
